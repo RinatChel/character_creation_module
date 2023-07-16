2dd4b6b0c5623583e951b91f055eddbe5cf5920d
Rinat Saifullin <saifullinrg@yandex.ru>
2023-07-16 17:44:02 +0500

    task done

--- main.py
+++ main.py
@@ -1,77 +1,80 @@
-from random import randint
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+    # ответ по умолчанию для всех одинаковый, можно
+    # доверить его родительскому классу
+    def answer_question(self, question):
+        print('Очень интересный вопрос! Не знаю.')
 
 
-def attack(char_name, char_class):
-    if char_class == 'warrior':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
-    if char_class == 'mage':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(5, 10)}')
-    if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику равный {5 + randint(-3, -1)}')
-def defence(char_name, char_class):
-    if char_class == 'warrior':
-        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
-    if char_class == 'mage':
-        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
-    if char_class == 'healer':
-        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
-def special(char_name, char_class):
-    if char_class == 'warrior': 
-        return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
-    if char_class == 'mage':
-        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
-    if char_class == 'healer':
-        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+class Student(Human):
+    def __init__(self, name):
+        super().__init__(name)
+
+    #  метод ask_question() принимает параметр someone:
+    #  это объект, экземпляр класса Curator, Mentor или CodeReviewer,
+    #  которому Student задаёт вопрос;
+    #  параметр question — это просто строка
+    #  имя объекта и текст вопроса задаются при вызове метода ask_question
+    def ask_question(self, someone, question):
+        # напечатайте на экран вопрос в нужном формате
+        print(f'{someone.name}, {question}')
+        # запросите ответ на вопрос у someone
+        someone.answer_question(question)
+        print()  # этот print выводит разделительную пустую строку
+
+
+class Curator(Human):
+    def answer_question(self, question):
+
+        if question == 'мне грустненько, что делать?':
+            print('Держись, всё получится. Хочешь видео с котиками?')
+        else:
+            super().answer_question(question)
+
+
+        # здесь нужно проверить, пришёл куратору знакомый вопрос или нет
+        # если да - ответить на него
+        # если нет - вызвать метод answer_question() у родительского класса
+
+# объявите и реализуйте классы CodeReviewer и Mentor
+class CodeReviewer(Human):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def answer_question(self, question):
+        if question == 'что не так с моим проектом?':
+            print('О, вопрос про проект, это я люблю.')
+        else:
+            super().answer_question(question)
+
+class Mentor(Human):
+    def __init__(self, name):
+        super().__init__(name)
+    def answer_question(self, question):
+
+        if question == 'мне грустненько, что делать?':
+            print('Отдохни и возвращайся с вопросами по теории.')
+        elif question == 'как устроиться работать питонистом?':
+            print('Сейчас расскажу.')
+
+        else:
+            super().answer_question(question)
 
 
 
 
-def start_training(char_name, char_class):
-    if char_class == 'warrior':
-        print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
-    if char_class == 'mage':
-        print(f'{char_name}, ты Маг — превосходный укротитель стихий.')
-    if char_class == 'healer':
-        print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
-    print('Потренируйся управлять своими навыками.')
-    print('Введи одну из команд: attack — чтобы атаковать противника, defence — чтобы блокировать атаку противника или special — чтобы использовать свою суперсилу.')
-    print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
-    while cmd != 'skip':
-        cmd = input('Введи команду: ')
-        if cmd == 'attack':
-            print(attack(char_name, char_class))
-        if cmd == 'defence':
-            print(defence(char_name, char_class))
-        if cmd == 'special':
-            print(special(char_name, char_class))
-    return 'Тренировка окончена.'
+# следующий код менять не нужно, он работает, мы проверяли
+student1 = Student('Тимофей')
+curator = Curator('Марина')
+mentor = Mentor('Ира')
+reviewer = CodeReviewer('Евгений')
+friend = Human('Виталя')
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
-    while approve_choice != 'y':
-        char_class = input('Введи название персонажа, за которого хочешь играть: Воитель — warrior, Маг — mage, Лекарь — healer: ')
-        if char_class == 'warrior':
-            print('Воитель — дерзкий воин ближнего боя. Сильный, выносливый и отважный.')
-        if char_class == 'mage':
-            print('Маг — находчивый воин дальнего боя. Обладает высоким интеллектом.')
-        if char_class == 'healer':
-            print('Лекарь — могущественный заклинатель. Черпает силы из природы, веры и духов.')
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, или любую другую кнопку, чтобы выбрать другого персонажа ').lower()
-    return char_class
-
-
-def main():
-    print('Приветствую тебя, искатель приключений!')
-    print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
-    print(f'Здравствуй, {char_name}! '
-          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
-    print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
-    print(start_training(char_name, char_class))
-    
-
-main()
+student1.ask_question(curator, 'мне грустненько, что делать?')
+student1.ask_question(mentor, 'мне грустненько, что делать?')
+student1.ask_question(reviewer, 'когда каникулы?')
+student1.ask_question(reviewer, 'что не так с моим проектом?')
+student1.ask_question(friend, 'как устроиться на работу питонистом?')
+student1.ask_question(mentor, 'как устроиться работать питонистом?')
